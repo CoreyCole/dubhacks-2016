cc4cc3aef410a7751853a8667b2e74b3b61afc3d
cryystalsong <cryystalsong@gmail.com>
2016-10-16 10:53:39 -0700

    fixed???

--- models/user.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-var userSchema = mongoose.Schema({
-  phone_number: String,
-  name: String,
-  birth_date: Date
-});
-
-module.exports = mongoose.model('User', userSchema);
